2e020e40b2f5e9f2c5958ad2158457b7bd0a6d53
CARobotics619 <carobotics@gmail.com>
2014-02-01 11:13:50 -0500

    commented the Thwacker object class and finished the isOut method

--- src/org/carobotics/subsystems/Thwacker.java
+++ src/org/carobotics/subsystems/Thwacker.java
@@ -1,32 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.carobotics.subsystems;
 
 import org.carobotics.hardware.Solenoid;
 
 /**
  *
+ * This class deals with the actual Thwacker object
+ * 
  * @author Student
  */
 public class Thwacker {
     protected Solenoid shooter1, shooter2;
     
+    //initiates the Thwacker object variables
     public Thwacker(Solenoid shooter1, Solenoid shooter2){
         this.shooter1 = shooter1;
         this.shooter2 = shooter2;
     }//end Thwacker object
     
+    //checks if the thwacker stick is outside of the robot
+    //or if it is set and ready to fire again
     public boolean isOut(Solenoid shooter){
-        if(shooter.get() = ){
-            
-        }//end if
-        return shooter;
+        
+        if(shooter.get()){
+            return true;
+        }else{
+            return false;
+        }//end if else
+        
     }//end boolean isOut
     
+    //returns one of the pneumatics of the shooter
     public Solenoid getShooter1(){
         return shooter1;
     }//end Solenoid getShooter1
@@ -35,4 +38,4 @@ public class Thwacker {
         return shooter2;
     }//end Solenoid getShooter1
     
-}
+}//end object Thwacker
